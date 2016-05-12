19158ebffecb20d000042bb4190e5e966ca9fe77
Ben Abbott <jaminscript@gmail.com>
2016-05-13 11:41:38 +1200

    Show cart contents and grand total

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -10,20 +10,21 @@ var cart = JSON.parse( localStorage.getItem('cart') );
 // Show the contents of the cart
 console.log( cart );
 
+// Show the user how many items they have in the cart
+updateCartDisplay();
+
 // Find all the add to cart buttons 
 var addToCartButtons = document.querySelectorAll('.add-to-cart');
 
 // Add click event listeners to them all
 for(var i=0; i<addToCartButtons.length; i++) {
-
 	addToCartButtons[i].onclick = addToCart;
-
 }
 
 function addToCart() {
 
 	var productName = this.dataset.name;
-	var productPrice = this.dataset.price;
+	var productPrice = parseFloat(this.dataset.price);
 
 	var product = {
 		name: productName,
@@ -34,7 +35,128 @@ function addToCart() {
 
 	localStorage.setItem('cart', JSON.stringify(cart) );
 
+	console.log(cart);
+
+	updateCartDisplay();
+
 }
+
+// Listen for clicks on the clear cart button
+document.querySelector('#clear-cart').onclick = function() {
+
+	// Reset localstorage
+	localStorage.setItem('cart', JSON.stringify( [] ));
+
+	// Reset the cart variable
+	cart = [];
+
+	updateCartDisplay();
+
+};
+
+
+function updateCartDisplay() {
+
+	// Get the cart contents
+	var cart = JSON.parse( localStorage.getItem('cart') );
+
+	// If the total items is 0
+	// if( cart.length == 0 ) {
+	// 	var text = '';
+	// } else {
+	// 	var text = cart.length;
+	// }
+
+	// Count the cart contents and display number on screen
+	document.querySelector('#total-cart').innerHTML = cart.length;
+
+	showCartTable();
+
+}
+
+function showCartTable() {
+
+	// Find the container that will hold the table
+	var container = document.querySelector('#cart-table');
+
+	// Create the HTML table
+	var table = document.createElement('table');
+
+	table.setAttribute('border', '1');
+
+	// Create a row to hold the headings
+	var headingsRow = document.createElement('tr');
+
+	// Create the name heading
+	var nameHeading = document.createElement('th');
+
+	nameHeading.innerHTML = 'Product Name';
+
+	// Create the price heading
+	var priceHeading = document.createElement('th');
+
+	priceHeading.innerHTML = 'Price';
+
+	// Add the headings to the headings row
+	headingsRow.appendChild(nameHeading);
+	headingsRow.appendChild(priceHeading);
+
+	// Add the headings row to the table
+	table.appendChild(headingsRow);
+
+	// Grand total
+	var grandTotal = 0;
+
+	// Loop over all the cart items
+	for(var i=0; i<cart.length; i++) {
+
+		// Get the price of the product and add to grand total
+		grandTotal += cart[i].price;
+
+		// Create a row for this product
+		var row = document.createElement('tr');
+
+		// Create the product name data element
+		var nameTD = document.createElement('td');
+
+		// Create the product price data element
+		var priceTD = document.createElement('td');
+
+		// Add data to the TD elements
+		nameTD.innerHTML = cart[i].name;
+		priceTD.innerHTML = cart[i].price;
+
+		// Add the TD elements to the row
+		row.appendChild(nameTD);
+		row.appendChild(priceTD);
+
+		// Add this row to the table
+		table.appendChild(row);
+
+	}
+
+	console.log(grandTotal);
+
+	var grandTotalRow = document.createElement('tr');
+	var grandTotalTD = document.createElement('td');
+	var fillerTD = document.createElement('td');
+
+	grandTotalTD.innerHTML = 'Grand Total: $' + grandTotal;
+
+	grandTotalRow.appendChild(fillerTD);
+	grandTotalRow.appendChild(grandTotalTD);
+
+	table.appendChild(grandTotalRow);
+
+
+	// Clear whatever is inside the div
+	container.innerHTML = '';
+
+	// Add the table to the screen
+	container.appendChild(table);
+
+}
+
 
 
 
